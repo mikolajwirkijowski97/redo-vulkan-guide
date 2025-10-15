831123a7738fb5db51d56a46e94b4ab9aae6f67a
Mikołaj Wirkijowski <lupsik@wp.pl>
2025-10-15 18:29:13 +0200

    Init vulkan instance and (phys.)device

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -8,6 +8,12 @@
 class VulkanEngine {
 public:
 
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 	bool stop_rendering{ false };
@@ -28,4 +34,11 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
 };
